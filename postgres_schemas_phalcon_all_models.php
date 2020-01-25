255b6dbcbe1749fb66636e8d488eb9fc7af5b3b4
Renato Gabriel <contas@renatogabriel.com>
2020-01-25 09:26:49 -0300

    Version as Component

--- postgres_schemas_phalcon_all_models.php
+++ postgres_schemas_phalcon_all_models.php
@@ -1,42 +1,84 @@
 <?php
+declare(strict_types = 1);
+namespace System\Component\Dev;
+
+use System\Injectable;
+use System\Helper\File;
 
 /**
- * Generate all models for each schema
- * on postgres.
- * 
- * @author renato
+ * Class HandleModels
  *
+ *
+ * @package System\Component
+ * @author renato gabriel
+ * @since 24/01/2020
+ * @version 1.0
  */
-class IndexController extends \Phalcon\Mvc\Controller
+class HandleModels extends Injectable
 {
-    
+
+    protected $modelOptions = [
+        '--force'
+    ];
+
+    protected $abstractOptions = [
+        '--doc',
+        'relations',
+        'fk',
+        'annotate',
+        'force',
+        'get-set',
+        'abstract',
+        'force'
+    ];
+
+    protected $sourceConfFile = PATH_CONFIG . DS . 'config_webtools.php';
+
     /**
-     * @param int $abstract => 1 generate abstract
-     * @param int $clear => 1 clear old before
-     * @param string $project
+     *
+     * @todo mudar ao mover por model, criar com o abstrato no model ja
+     *      
+     * @var string
      */
-    public function modelsAction(int $abstract = 0, int $clear = 0, string $project = 'project-name'): void
-    {       
+    protected $extends = '\\\System\\\Mvc\\\Model';
+
+    protected $data;
+
+    /**
+     *
+     * @todo move to phalcon cli
+     * @todo do it for each model in each schema instead of each schema.
+     * @todo for all projects if $project is null
+     * @todo choose schemas.
+     *      
+     * @param bool $abstract
+     * @param bool $clear
+     * @param array $skip
+     * @param array $project
+     * @return array
+     */
+    public function __construct(bool $abstract = false, bool $clear = false, array $skip = [], array $project = [
+        'vztus-cms'
+    ])
+    {
         $sql = "SELECT nspname as schema FROM pg_catalog.pg_namespace WHERE nspname NOT IN('pg_toast','pg_temp_1','pg_toast_temp_1','pg_catalog','public','information_schema')";
         $fetchSchemas = $this->db->fetchAll($sql);
         $commands = [
-            'cd /var/www/html/' . $project
+            'cd /var/www/html/' . $project[0]
         ];
-        
+
         foreach ($fetchSchemas as $row) {
             $schema = $row['schema'];
             $schemaUc = ucfirst($row['schema']);
-            
-            $configFile = realpath(PATH_CONFIG . DS . 'config_webtools.php');
-            
-            $extends = '\\\System\\\Mvc\\\Model';
-            if ($abstract == 1) {
+
+            $configFile = realpath($this->sourceConfFile);
+
+            if (true === $abstract) {
                 $dir = realpath(PATH_MODELS . DS . 'Abstracts' . DS . $schemaUc);
                 $nameSpace = sprintf('App\\\Models\\\Abstracts\\\%s', $schemaUc);
                 if (! realpath(PATH_MODELS . DS . 'Abstracts')) {
                     File::criarDir(PATH_MODELS . DS . 'Abstracts');
                 }
-                
                 if (! $dir) {
                     File::criarDir(PATH_MODELS . DS . 'Abstracts' . DS . $schemaUc);
                     $dir = realpath(PATH_MODELS . DS . 'Abstracts' . DS . $schemaUc);
@@ -49,8 +91,8 @@ class IndexController extends \Phalcon\Mvc\Controller
                     $dir = realpath(PATH_MODELS . DS . $schemaUc);
                 }
             }
-            
-            if ($clear and ($schema != 'system' or $abstract == 1)) {
+
+            if (true === $clear and ! in_array($schema, $skip)) {
                 $di = new \RecursiveDirectoryIterator($dir, \FilesystemIterator::SKIP_DOTS);
                 $ri = new \RecursiveIteratorIterator($di, \RecursiveIteratorIterator::CHILD_FIRST);
                 foreach ($ri as $file) {
@@ -59,36 +101,29 @@ class IndexController extends \Phalcon\Mvc\Controller
                     }
                     unlink($file);
                 }
+            } else if (in_array($schema, $skip)) {
+                continue;
             }
-            
-            if ($abstract == 1) {
-                $options = [
-                    '--doc',
-                    'relations',
-                    'fk',
-                    'annotate',
-                    'force',
-                    'get-set',
-                    'abstract'
-                ];
-            } else {
-                $options = [
-                    '--force'
-                ];
-            }
-            
-            $commands[] = sprintf('phalcon all-models --schema=%s --namespace=%s --extends=%s --output=%s --config=%s %s 2>&1', $schema, $nameSpace, $extends, $dir, $configFile, join(' --', $options));
+
+            $commands[] = sprintf('phalcon all-models --schema=%s --namespace=%s --extends=%s --output=%s --config=%s %s 2>&1', $schema, $nameSpace, $this->extends, $dir, $configFile, join(' --', (true === $abstract) ? $this->abstractOptions : $this->modelOptions));
         }
+
+        $verbose = null;
+        $returnValue = null;
         exec(join(' && ', $commands), $verbose, $returnValue);
-        
+
         $verbose = array_filter($verbose);
         $return = [
             'return' => $returnValue,
             'command' => $commands,
             'log' => $verbose
         ];
-        
-        var_dump($return);
-        exit();
+
+        $this->data = $return;
+    }
+
+    public function getResponse()
+    {
+        return $this->data;
     }
 }
